d17012ed20951d7cfcf11a9ac368875f4a2fa286
frmuvto <124471166+frmuvto@users.noreply.github.com>
2023-02-07 21:59:00 -0500

    Add files via upload

---
+++ instructions1.h
@@ -0,0 +1,51 @@
+
+
+int plus(int int1, int int2){
+    result = (int1 + int2);
+    return result;
+}
+
+int plusi(int int1, int int2){
+    result = (int1 + int2);
+    return result;
+}
+
+int minus(int int1, int int2){
+    result = (int1 - int2);
+    return result;
+}
+
+int minui(int int1, int int2){
+    result = (int1 - int2);
+    return result;
+}
+
+int times(int int1, int int2){
+    result = (int1 * int2);
+    return result;
+}
+
+int timei(int int1, int int2){
+    result = (int1 * int2);
+    return result;
+}
+
+int div(int int1, int int2){
+    result = (int1 / int2);
+    return result;
+}
+
+int divd(int int1, int int2){
+    result = (int1 / int2);
+    return result;
+}
+
+int mod(int int1, int int2){
+    result = (int1 % int2);
+    return result;
+}
+
+int modi(int int1, int int2){
+    result = (int1 % int2);
+    return result;
+}
--- parser.c
+++ parser.c
@@ -1,64 +1,74 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+//#include <instructions1.h>
 
 #define max_length 24
 
-int find_reg(char* arg){
+int find_reg(char* arg, char* reg_hex){
 
     int constant;
 
     if(strcmp(arg, "r0") == 1){
 
         return 0;
+        reg_hex = "1";
 
     }
 
     else if(strcmp(arg, "r1") == 1){
 
         return 1;
+        reg_hex = "2";
 
     }
 
     else if(strcmp(arg, "r2") == 1){
 
         return 2;
+        reg_hex = "3";
 
     }
 
     else if(strcmp(arg, "r3") == 1){
 
         return 3;
+        reg_hex = "4";
 
     }
     
     else if(strcmp(arg, "r4") == 1){
 
         return 4;
+        reg_hex = "5";
 
     }
     
     else if(strcmp(arg, "r5") == 1){
 
         return 5;
+        reg_hex = "6";
 
     }
     
     else if(strcmp(arg, "r6") == 1){
 
         return 6;
+        reg_hex = "7";
 
     }
 
     else if(strcmp(arg, "r7") == 1){
 
         return 7;
+        reg_hex = "8";
 
     }
 
     else{
 
         return -1;
+        reg_hex = "0";
 
     }
     
@@ -126,9 +136,9 @@ int call_instruction(char* instruction, int value1, int value2, int value3){
 
     }
 
-    else if(strcmp(instruction, "div") == 1){
+    else if(strcmp(instruction, "divd") == 1){
 
-        result = div(value2, value3);
+        result = divd(value2, value3);
 
         return result;
 
@@ -238,9 +248,9 @@ int call_instruction(char* instruction, int value1, int value2, int value3){
 
     }
 
-    else if(strcmp(instruction, "exp") == 1){
+    else if(strcmp(instruction, "expn") == 1){
 
-        result = exp(value2, value3);
+        result = expn(value2, value3);
 
         return result;
 
@@ -348,9 +358,9 @@ int call_instruction(char* instruction, int value1, int value2, int value3){
 
     }
 
-    else if(strcmp(instruction, "log") == 1){
+    else if(strcmp(instruction, "logr") == 1){
 
-        result = log(value2, value3);
+        result = logr(value2, value3);
 
         return result;
 
@@ -404,7 +414,7 @@ int call_instruction(char* instruction, int value1, int value2, int value3){
 
 }
 
-int find_instruction_type(char* instruction){
+int find_instruction_type(char* instruction, char* instruction_hex){
 
     //integer representing argument type
     int type;
@@ -421,246 +431,288 @@ int find_instruction_type(char* instruction){
     if(strcmp(instruction, "plus") == 1){
 
         return 1;
+        instruction_hex = "01";
 
     }
 
     else if(strcmp(instruction, "plusi") == 1){
 
         return 1;
+        instruction_hex = "02";
+        
 
     }
 
     else if(strcmp(instruction, "avg") == 1){
 
         return 1;
+        instruction_hex = "03";
 
     }
 
     else if(strcmp(instruction, "minus") == 1){
 
         return 1;
+        instruction_hex = "04";
 
     }
 
     else if(strcmp(instruction, "minui") == 1){
 
         return 2;
+        instruction_hex = "05";
 
     }
 
     else if(strcmp(instruction, "times") == 1){
 
         return 1;
+        instruction_hex = "06";
 
     }
 
     else if(strcmp(instruction, "timei") == 1){
 
         return 2;
+        instruction_hex = "07";
 
     }
 
-    else if(strcmp(instruction, "div") == 1){
+    else if(strcmp(instruction, "divd") == 1){
 
         return 1;
+        instruction_hex = "08";
 
     }
 
     else if(strcmp(instruction, "divi") == 1){
 
         return 2;
+        instruction_hex = "09";
 
     }
 
     else if(strcmp(instruction, "mod") == 1){
 
         return 1;
+        instruction_hex = "0A";
 
     }
 
     else if(strcmp(instruction, "modi") == 1){
 
         return 2;
+        instruction_hex = "0B";
 
     }
 
     else if(strcmp(instruction, "div4") == 1){
 
         return 5;
+        instruction_hex = "0C";
 
     }
 
     else if(strcmp(instruction, "mult4") == 1){
 
         return 5;
+        instruction_hex = "0D";
 
     }
 
     else if(strcmp(instruction, "equal") == 1){
 
         return 1;
+        instruction_hex = "0E";
 
     }
 
     else if(strcmp(instruction, "noteq") == 1){
 
         return 1;
+        instruction_hex = "0F";
 
     }
 
     else if(strcmp(instruction, "less") == 1){
 
         return 1;
+        instruction_hex = "10";
 
     }
 
     else if(strcmp(instruction, "great") == 1){
 
         return 1;
+        instruction_hex = "11";
 
     }
 
     else if(strcmp(instruction, "isneg") == 1){
 
         return 5;
+        instruction_hex = "12";
 
     }
 
     else if(strcmp(instruction, "ispos") == 1){
 
         return 5;
+        instruction_hex = "13";
 
     }
 
     else if(strcmp(instruction, "iseve") == 1){
 
         return 5;
+        instruction_hex = "14";
 
     }
 
     else if(strcmp(instruction, "isodd") == 1){
 
         return 5;
+        instruction_hex = "15";
 
     }
 
-    else if(strcmp(instruction, "exp") == 1){
+    else if(strcmp(instruction, "expn") == 1){
 
         return 1;
+        instruction_hex = "16";
 
     }
 
     else if(strcmp(instruction, "expi") == 1){
 
         return 2;
+        instruction_hex = "17";
 
     }
 
     else if(strcmp(instruction, "prim") == 1){
 
         return 1;
+        instruction_hex = "18";
 
     }
 
     else if(strcmp(instruction, "set") == 1){
 
         return 3;
+        instruction_hex = "19";
 
     }
 
     else if(strcmp(instruction, "neg") == 1){
 
         return 5;
+        instruction_hex = "1A";
 
     }
 
     else if(strcmp(instruction, "addao") == 1){
 
         return 1;
+        instruction_hex = "1B";
 
     }
 
     else if(strcmp(instruction, "addat") == 1){
 
         return 1;
+        instruction_hex = "1C";
 
     }
 
     else if(strcmp(instruction, "copy") == 1){
 
         return 5;
+        instruction_hex = "1D";
 
     }
 
     else if(strcmp(instruction, "lessi") == 1){
 
         return 2;
+        instruction_hex = "1E";
 
     }
 
     else if(strcmp(instruction, "greti") == 1){
 
         return 2;
+        instruction_hex = "1F";
 
     }
 
     else if(strcmp(instruction, "minao") == 1){
 
         return 1;
+        instruction_hex = "20";
 
     }
 
     else if(strcmp(instruction, "clear") == 1){
 
         return 3;
+        instruction_hex = "21";
 
     }
 
     else if(strcmp(instruction, "fact") == 1){
 
         return 1;
+        instruction_hex = "22";
 
     }
 
     else if(strcmp(instruction, "facti") == 1){
 
         return 2;
+        instruction_hex = "23";
 
     }
 
-    else if(strcmp(instruction, "log") == 1){
+    else if(strcmp(instruction, "logr") == 1){
 
         return 1;
+        instruction_hex = "24";
 
     }
 
     else if(strcmp(instruction, "nlog") == 1){
 
         return 1;
+        instruction_hex = "25";
 
     }
 
     else if(strcmp(instruction, "fctol") == 1){
 
         return 5;
+        instruction_hex = "26";
 
     }
 
     else if(strcmp(instruction, "fctoi") == 1){
 
         return 3;
+        instruction_hex = "27";
 
     }
 
     else if(strcmp(instruction, "perm") == 1){
 
-        return 1;
+        return 3;
+        instruction_hex = "28";
 
     }
 
     else if(strcmp(instruction, "permi") == 1){
 
-        return 2;
+        return 3;
+        instruction_hex = "29";
 
     }
 
@@ -672,20 +724,12 @@ int find_instruction_type(char* instruction){
 
 }
 
-void integer_to_binary(int integer){
-    
-    char binary [32];
-
-    return binary;
-
-}
-
 
 int main() {
 
     //declare array for registers
     int registerCount = 8;
-    int regArray[registerCount];
+    int regArray[8] = {1, 3, 5, 7, 9, 11, 13, 15};
     int arguments[2];
 
     char* instruction;
@@ -699,6 +743,12 @@ int main() {
 
     int type;
 
+    char instruction_code[2];
+    char reg1_code[1];
+    char reg2_code[1];
+    char reg3_code[1];
+    char const_code[8];
+
     //declare input string
     char inputString[max_length];
 
@@ -706,6 +756,7 @@ int main() {
 
     printf("Current value of registers:\n", inputString);
         for (int i = 0; i < registerCount; i++){
+            printf("r%d:", i);
             printf("%d, ", regArray[i]);
         }
     
@@ -729,7 +780,7 @@ int main() {
         }
 
         //parse first argument, which must always be a valid register
-        value1 = find_reg(arg1);
+        value1 = find_reg(arg1, reg1_code);
 
         if (value1 < 0 || value1 >= registerCount){
 
@@ -742,15 +793,13 @@ int main() {
 
     //Prepare values for different instruction types. The atoi function is technically obsolete but should work for the purposes of this project.
 
-        type = find_instruction_type(instruction);
-
     //type 1: three registers
 
-        if (find_instruction_type(instruction) == 1){
+        if (find_instruction_type(instruction, instruction_code) == 1){
 
             arguments[0] = regArray[value1];
 
-            value2 = find_reg(arg2);
+            value2 = find_reg(arg2, reg2_code);
 
             if (value2 < 0 || value2 >= registerCount){
 
@@ -761,7 +810,7 @@ int main() {
 
             arguments[1] = regArray[value2];
 
-            value3 = find_reg(arg3);
+            value3 = find_reg(arg3, reg3_code);
 
             if (value3 < 0 || value3 >= registerCount){
 
@@ -776,11 +825,11 @@ int main() {
         
     //type 2: 2 registers + 1 constant
 
-        else if(find_instruction_type(instruction) == 2){
+        else if(find_instruction_type(instruction, instruction_code) == 2){
 
             arguments[0] = regArray[value1];
 
-            value2 = find_reg(arg2);
+            value2 = find_reg(arg2, reg2_code);
 
             if (value2 < 0 || value2 >= registerCount){
 
@@ -793,20 +842,24 @@ int main() {
 
             arguments[2] = atoi(arg3);
 
+            sprintf(const_code, "%x", arguments[2]);
+
         }
     //type 3: 1 register + 1 constant
 
-        else if(find_instruction_type(instruction) == 3){
+        else if(find_instruction_type(instruction, instruction_code) == 3){
 
             arguments[0] = regArray[value1];
 
             arguments[1] = atoi(arg2);
 
+            sprintf(const_code, "%x", arguments[1]);
+
             arguments[2] = 0;//should not be used in this case
         }
-    //type 4: 1 register + 2 constants
+    /*type 4: 1 register + 2 constants
 
-        else if(find_instruction_type(instruction) == 4){
+        else if(find_instruction_type(instruction, instruction_code) == 4){
 
             arguments[0] = regArray[value1];
 
@@ -814,14 +867,14 @@ int main() {
 
             arguments[2] = atoi(arg3);
 
-        }
+        }*/
     //type 5: 1 register + 1 register
 
-        else if(find_instruction_type(instruction) == 5){
+        else if(find_instruction_type(instruction, instruction_code) == 5){
 
             arguments[0] = regArray[value1];
 
-            value2 = find_reg(arg2);
+            value2 = find_reg(arg2, reg2_code);
 
             if (value2 < 0 || value2 >= registerCount){
 
@@ -844,6 +897,12 @@ int main() {
 
         }
 
+        //print hex instruction code
+        printf ("%s", instruction_code);
+        printf ("%s", reg1_code);
+        printf ("%s", reg2_code);
+        printf ("%s", reg3_code);
+        printf ("%s", const_code);
 
 
         regArray[value1] = call_instruction(instruction, arguments[0], arguments[1], arguments[2]);
