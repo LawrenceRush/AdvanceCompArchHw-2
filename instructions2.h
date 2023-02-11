c868e64b6b63a477e176f0637c8131c01251c991
syracu <124523495+syracu@users.noreply.github.com>
2023-02-11 08:39:08 +0530

    Add files via upload

---
+++ instructions2.h
@@ -0,0 +1,74 @@
+// instructions for log10, loge, factorial integer, factorial register , isodd , iseven,ispositive, isnegative,great (return 1 if value1>value2 , less (return 1 if value1< value2)
+//#include <stdio.h>
+#include <math.h>
+
+int logr(int x) {
+//	printf("\n%f\n",log10((double) x));
+   int result = 0;
+     while (x >= 10) {
+
+        result += 1;
+    }
+	    return result;
+}
+int nlog(int x) {
+    double result = log((double)x);
+	//printf("\n%f\n",result);
+	  return result;
+}
+int fctoi(int n) {
+    int result = 1;
+    for (int i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+int fctol(int reg){
+	int result=1;
+	for (int i = 2; i <= reg; i++) {
+        result *= i;
+    }
+	    return result;
+}
+int isodd(int n) {
+    if (n % 2 == 0) {
+        return 0;
+    } else {
+		return 1;
+	}
+}
+int iseve(int n) {
+    if (n % 2 == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+int ispos(int n) {
+    if (n > 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+int isneg(int n) {
+    if (n < 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+int great(int x, int y) {
+    if (x > y) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+int less(int x, int y) {
+    if (x < y) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
